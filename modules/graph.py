17ce4f9281b8bf5a876e5464e2c4f004a0fa4824
valesteban <valentina.esteban@ug.uchile.cl>
2024-08-20 20:25:33 -0400

    creación nodes.csv por medio de lista de feat

--- modules/graph.py
+++ modules/graph.py
@@ -96,7 +96,7 @@ class Graph:
 
         
 
-    def node_features_nodes(self,features_filename, list_feat = "all"):
+    def features_nodes(self,features_filename, list_feat = "all"):
         """
         Crea archivo nodes.csv con los nodos y sus features.
         Si no se le pasa la lista de features se asumen que se crea con todo
@@ -104,22 +104,42 @@ class Graph:
         
         # TODO: hacer que se pueda ir variando los features de nodos GRP SEARCH
         # FIXME: tengo que agregar la creacion de nx_graph al final de la funcion read_from_relationship_edgelist(...)
+        
+        # Lee el archivo de características
         features = pd.read_csv(features_filename)
+
+        # Si se especifica una lista de features, selecciona solo esas columnas
+        if list_feat != "all":
+
+            # Crear una lista para guardar las columnas seleccionadas
+            selected_columns = ['ASN']
+            
+            # Iterar sobre cada prefijo en list_feat (para representacion de hot hot de feat categoricos)
+            for prefix in list_feat:
+                # Filtrar columnas que empiezan con el prefijo
+                matched_columns = [col for col in features.columns if col.startswith(prefix)]
+                selected_columns.extend(matched_columns)
+            
+            # Filtrar el DataFrame para obtener solo columnas seleccionadas
+            features = features[selected_columns]
+            
+        # Crea el archivo nodes.csv
+        f = open(self.path + "nodes.csv", "w")
         
-        # Creo archivo
-        f = open(self.path + "nodes.csv","w")
-        
-        # Agregamos headers
+        # Agrega los headers
         headers = "node_id,feat\n"
         f.write(headers)
 
-        # Por cada nodo en la topología lo agrego en el archivo nodes.csv con sus features 
+        # Por cada nodo en la topología, lo agrego en el archivo nodes.csv con sus features
         for node in self.nx_graph.nodes():
+            # Filtra las filas correspondientes al nodo y obtiene los features seleccionados
             node_features = features.loc[features['ASN'] == int(node)].fillna(0).to_numpy()[0].tolist()[1:]
             node_features = ', '.join([str(feature) for feature in node_features])
             w = f'{str(node)},"{node_features}"\n'
             f.write(w)
-    
+        
+        f.close()
+
     def only_degree_features_nodes(self, features_filename):
         """
         Crea archivo nodes.csv el cual consiste en node_id y feats lo que consiste en degree_node_in y degree_node_out
@@ -163,7 +183,8 @@ class Graph:
         )
         df_edges.to_csv(self.path + "edges.csv", index=False)
 
-        print(self.nx_graph)
+        if self.debug:
+            print(self.nx_graph)
 
 TOR_LABELS_DICT = {'P2P':0, 'C2P': 1,'P2C': 2}
 class_names = ['P2P', 'C2P', 'P2C']
