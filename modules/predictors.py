3b5e2dff549d9b15ca19140b49a28b4b93c3d90f
valesteban <valentina.esteban@ug.uchile.cl>
2024-10-24 10:00:12 -0300

    MLP Predictor para embeddings

--- modules/predictors.py
+++ modules/predictors.py
@@ -44,3 +44,39 @@ class MLPPredictor(nn.Module):
             g.ndata['h'] = h
             g.apply_edges(self.apply_edges)
             return g.edata['score']
+
+
+class MLPPredictorEmbeddings(nn.Module):
+    def __init__(self, h_feats,out_feats):
+        super().__init__()
+        self.W1 = nn.Linear(h_feats * 2, h_feats)
+        self.W2 = nn.Linear(h_feats, out_feats)
+
+    def apply_edges(self, edges):
+        """
+        Computes a scalar score for each edge of the given graph.
+
+        Parameters
+        ----------
+        edges :
+            Has three members ``src``, ``dst`` and ``data``, each of
+            which is a dictionary representing the features of the
+            source nodes, the destination nodes, and the edges
+            themselves.
+
+        Returns
+        -------
+        dict
+            A dictionary of new edge features.
+        """
+        h_src = edges.src['h']  # Obtener los embeddings del nodo fuente
+        h_dst = edges.dst['h']
+        h = torch.cat([h_src, h_dst], 1)
+        return {'score': self.W2(F.relu(self.W1(h))).squeeze(1)}
+
+    def forward(self, g, emb):
+        with g.local_scope():
+            # Obtener los embeddings para los nodos dados
+            g.ndata['h'] = emb
+            g.apply_edges(self.apply_edges)
+            return g.edata['score']
